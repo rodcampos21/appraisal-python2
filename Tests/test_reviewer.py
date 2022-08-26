966b08c23737a01aa22d14a4e8732fcb07a40f7b
llcruz1 <lucaslimacruz1@gmail.com>
2022-08-25 22:54:43 -0300

    Refatora testes unitários do módulo Reviewer

--- Tests/test_reviewer.py
+++ Tests/test_reviewer.py
@@ -1,99 +1,110 @@
-""" For me (Leonardo) to be able to use reviewer locally
-    Change path as needed """
-# import sys
-# sys.path.insert(0, '/Users/super/Desktop/Projeto_Appraisal')
-"""  """
-
 import unittest
 from sklearn.metrics import mean_squared_error
-from reviewer import (
-    reviewer,
-    reviewer_euc,
-    reviewer_mah,
-    reviewer_man,
-    reviewer_min,
-    reviewer_mse,
-)
+from reviewer import Reviewer
+from Strategy.MeasureStrategy import MSE, EUC, MAH, MAN
 import pandas as pd
 import scipy.spatial.distance as sp
 import numpy as np
 
 
 class test_reviewer(unittest.TestCase):
-    """(for the sake of testing, the iris_filled file has been created already with the crowner)"""
 
     @classmethod
     def setUpClass(cls):
         cls.original_file = pd.read_csv("iris.csv")
-        cls.filled_file = pd.read_csv("iris_filled.csv")
+        cls.original_file_name = "iris.csv"
+        cls.filled_file = pd.read_csv("Tests/iris_filled.csv")
+        cls.filled_file_name="Tests/iris_filled.csv"
 
     def test_mse(cls):
 
         """Checks to see if the column returns the correct MSE value"""
-
-        result = reviewer_mse(
-            cls.original_file[["sepal.length"]], cls.filled_file[["sepal.length"]]
-        )
+        
+        # Arrange
         expected_result = mean_squared_error(
             cls.filled_file[["sepal.length"]], cls.original_file[["sepal.length"]]
         )
-        cls.assertEqual(result, expected_result)
+
+        reviewer = Reviewer(MSE(), "sepal.length")
+
+        # Act
+        result = reviewer(
+            cls.original_file_name, cls.filled_file_name
+        )
+
+        # Assert
+        cls.assertAlmostEqual(result._output, expected_result)
+
 
     def test_euc(cls):
 
         """Checks to see if the column returns the correct Euclidian distance"""
-
-        result = reviewer_euc(
-            cls.original_file[["sepal.length"]], cls.filled_file[["sepal.length"]]
-        )
+        
+        #  Arrange
         expected_result = sp.euclidean(
             cls.filled_file[["sepal.length"]], cls.original_file[["sepal.length"]]
         )
-        cls.assertEqual(result, expected_result)
 
-    def test_min(cls):
+        reviewer = Reviewer(EUC(), "sepal.length")
 
-        """Checks to see if the column returns the correct Minkowski distance"""
-
-        result = reviewer_min(
-            cls.original_file[["sepal.length"]], cls.filled_file[["sepal.length"]]
+        # Act
+        result = reviewer(
+            cls.original_file_name, cls.filled_file_name
         )
-        expected_result = sp.minkowski(
-            cls.filled_file[["sepal.length"]], cls.original_file[["sepal.length"]]
-        )
-        cls.assertEqual(result, expected_result)
+
+        # Assert
+        cls.assertAlmostEqual(result._output, expected_result)
+
 
     def test_man(cls):
 
         """Checks to see if the column returns the correct Manhattan distance"""
 
-        result = reviewer_man(
-            cls.original_file[["sepal.length"]], cls.filled_file[["sepal.length"]]
-        )
+        # Arrange
         expected_result = sp.cityblock(
             cls.filled_file[["sepal.length"]], cls.original_file[["sepal.length"]]
         )
-        cls.assertEqual(result, expected_result)
+
+        reviewer = Reviewer(MAN(), "sepal.length")
+
+        # Act
+        result = reviewer(
+            cls.original_file_name, cls.filled_file_name
+        )
+
+        # Assert
+        cls.assertAlmostEqual(result._output, expected_result)
+
 
     def test_mah(cls):
 
         """Checks to see if the column returns the correct Mahalanobis distance"""
+        
+        # Arrange
 
-        result = reviewer_mah(
-            cls.original_file[["sepal.length"]], cls.filled_file[["sepal.length"]]
-        )
+        reviewer = Reviewer(MAH(), "sepal.length")
+
         cov_mat = np.stack(
             (cls.original_file[["sepal.length"]], cls.filled_file[["sepal.length"]]),
             axis=1,
         ).squeeze()
+
         cov = np.cov(cov_mat)
         inv_cov = np.linalg.pinv(cov)
+        
         expected_result = sp.mahalanobis(
             cls.original_file[["sepal.length"]],
             cls.filled_file[["sepal.length"]],
             inv_cov,
         )
-        cls.assertEqual(result, expected_result)
+
+        # Act
+        result = reviewer(
+            cls.original_file_name, cls.filled_file_name
+        )
+
+        # Arrange
+        cls.assertAlmostEqual(result._output, expected_result)
 
 
 if __name__ == "__main__":
